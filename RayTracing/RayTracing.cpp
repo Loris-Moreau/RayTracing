6349845eb4873ecd86802dcb0ccbdbbbdb51cfca
Loris M <80474693+Loris-Moreau@users.noreply.github.com>
2024-01-15 09:45:01 +0100

    [add] PPM File
    Colors !!!!!

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -1,6 +1,7 @@
 // RayTracing.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
 #include <iostream>
+#include "Color.h"
 
 using namespace std;
 
@@ -8,15 +9,23 @@ int main(int argc, char* argv[])
 {
     //Resolution
     int width = 256, height = 256;
+
     //Image
     cout << "P3\n" << width << ' ' << height << "\n255\n";
+
     for (int y = 0; y < height; y++)
     {
+        clog << "Progress : " << (y * 100 / height) << " %\n" << flush;
+
         for (int x = 0; x < width; x++)
         {
-            //Simple gray Color
-            cout << "120 120 120" << endl;
+            Color pixel(double(x) / (width - 1), double(y) / (height - 1), 1 - (double(x) / (width - 1)));
+
+            WriteColor(cout, pixel);
         }
     }
+
+    clog << "It has been done \n";
+
     return 0;
 }
