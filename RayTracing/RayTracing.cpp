b2f8ff9113eadfc7496979d97e9d16bb3eec56c8
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2025-05-07 16:34:25 +0200

    [fix] spacing
    it was annoying me... :3

--- RayTracing/RayTracing.cpp
+++ RayTracing/RayTracing.cpp
@@ -91,36 +91,36 @@ HittableList random_scene()
             }
         }
     }
-
+    
     world.add(std::make_shared<Sphere>(Vector3{0, 1, 0}, 1.0, std::make_shared<Dielectric>(1.5)));
     world.add(std::make_shared<Sphere>(Vector3{-4, 1, 0}, 1.0, std::make_shared<Lambertian>(std::make_shared<SolidColor>(0.4, 0.2, 0.1))));
     world.add(std::make_shared<Sphere>(Vector3{4, 1, 0}, 1.0, std::make_shared<Metal>(Vector3{0.7, 0.6, 0.5}, 0.0)));
-
+    
     return world;
 }
 
 HittableList two_spheres()
 {
     HittableList objects;
-
+    
     std::shared_ptr<CheckerTexture> checker = std::make_shared<CheckerTexture>(std::make_shared<SolidColor>(0.2, 0.3, 0.1),
                                                                                std::make_shared<SolidColor>(0.9, 0.9, 0.9));
-
+    
     objects.add(std::make_shared<Sphere>(point3(0, -10, 0), 10, std::make_shared<Lambertian>(checker)));
     objects.add(std::make_shared<Sphere>(point3(0, 10, 0), 10, std::make_shared<Lambertian>(checker)));
-
+    
     return objects;
 }
 
 HittableList two_perlin_spheres()
 {
     HittableList objects;
-
+    
     const std::shared_ptr<NoiseTexture> pertext = std::make_shared<NoiseTexture>(4.0);
-
+    
     objects.add(std::make_shared<Sphere>(point3(0, -1000, 0), 1000, std::make_shared<Lambertian>(pertext)));
     objects.add(std::make_shared<Sphere>(point3(0, 2, 0), 2, std::make_shared<Lambertian>(pertext)));
-
+    
     return objects;
 }
 
@@ -129,34 +129,34 @@ HittableList earth()
     std::shared_ptr<ImageTexture> earth_texture = std::make_shared<ImageTexture>("Images/earthmap.jpg");
     std::shared_ptr<Lambertian> earth_surface = std::make_shared<Lambertian>(earth_texture);
     const std::shared_ptr<Sphere> globe = std::make_shared<Sphere>(point3(0, 0, 0), 2, earth_surface);
-
+    
     return HittableList(globe);
 }
 
 HittableList simple_light()
 {
     HittableList objects;
-
+    
     std::shared_ptr<NoiseTexture> pertext = std::make_shared<NoiseTexture>(4);
     objects.add(std::make_shared<Sphere>(point3(0, -1000, 0), 1000, std::make_shared<Lambertian>(pertext)));
     objects.add(std::make_shared<Sphere>(point3(0, 2, 0), 2, std::make_shared<Lambertian>(pertext)));
-
+    
     std::shared_ptr<DiffuseLight> difflight = std::make_shared<DiffuseLight>(std::make_shared<SolidColor>(4, 4, 4));
     objects.add(std::make_shared<Sphere>(point3(0, 7, 0), 2, difflight));
     objects.add(std::make_shared<XYRect>(3, 5, 1, 3, -2, difflight));
-
+    
     return objects;
 }
 
 HittableList cornell_box()
 {
     HittableList objects;
-
+    
     std::shared_ptr<Lambertian>   red = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.65, .05, .05));
     std::shared_ptr<Lambertian>   white = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.73, .73, .73));
     std::shared_ptr<Lambertian>   green = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.12, .45, .15));
     std::shared_ptr<DiffuseLight> light = std::make_shared<DiffuseLight>(std::make_shared<SolidColor>(15, 15, 15));
-
+    
     objects.add(std::make_shared<FlipFace>(std::make_shared<YZRect>(0, 555, 0, 555, 555, green)));
     objects.add(std::make_shared<YZRect>(0, 555, 0, 555, 0, red));
     objects.add(std::make_shared<XZRect>(213, 343, 227, 332, 554, light));
@@ -176,19 +176,18 @@ HittableList cornell_box()
     box2 = std::make_shared<Translate>(box2, Vector3(130, 0, 65));
     objects.add(std::move(box2));
     
-    
     return objects;
 }
 
 HittableList cornell_smoke()
 {
     HittableList objects;
-
+    
     std::shared_ptr<Lambertian>   red = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.65, .05, .05));
     std::shared_ptr<Lambertian>   white = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.73, .73, .73));
     std::shared_ptr<Lambertian>   green = std::make_shared<Lambertian>(std::make_shared<SolidColor>(.12, .45, .15));
     std::shared_ptr<DiffuseLight> light = std::make_shared<DiffuseLight>(std::make_shared<SolidColor>(7, 7, 7));
-
+    
     objects.add(std::make_shared<FlipFace>(std::make_shared<YZRect>(0, 555, 0, 555, 555, green)));
     objects.add(std::make_shared<YZRect>(0, 555, 0, 555, 0, red));
     objects.add(std::make_shared<XZRect>(113, 443, 127, 432, 554, light));
@@ -201,14 +200,14 @@ HittableList cornell_smoke()
     std::shared_ptr<Hittable> box1 = std::make_shared<Box>(point3(0, 0, 0), point3(165, 330, 165), white);
     box1 = std::make_shared<RotateY>(box1, 15);
     box1 = std::make_shared<Translate>(box1, Vector3(265, 0, 295));
-
+    
     std::shared_ptr<Hittable> box2 = std::make_shared<Box>(point3(0, 0, 0), point3(165, 165, 165), white);
     box2 = std::make_shared<RotateY>(box2, -18);
     box2 = std::make_shared<Translate>(box2, Vector3(130, 0, 65));
-
+    
     objects.add(std::make_shared<ConstantMedium>(box1, 0.01, std::make_shared<SolidColor>(0, 0, 0)));
     objects.add(std::make_shared<ConstantMedium>(box2, 0.01, std::make_shared<SolidColor>(1, 1, 1)));
-
+    
     return objects;
 }
 
@@ -229,7 +228,7 @@ HittableList final_scene()
             const double x1 = x0 + w;
             const double y1 = random_double(1, 101);
             const double z1 = z0 + w;
-
+            
             boxes1.add(std::make_shared<Box>(point3(x0, y0, z0), point3(x1, y1, z1), ground));
         }
     }
@@ -282,11 +281,11 @@ int main(int argc, char* argv[])
     const int samples_per_pixel = 10000;
     const int max_depth = 50;
     const double aspect_ratio = static_cast<double>(image_width) / image_height;
-
+    
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
-
+    
     const HittableList world = final_scene();
-
+    
     const Vector3 lookfrom{478, 278, -800};
     const Vector3 lookat{278, 278, 0};
     const Vector3 vup{0, 1, 0};
@@ -294,9 +293,9 @@ int main(int argc, char* argv[])
     const double aperture = 0.0;
     const double vfov = 40.0;
     const color background{0, 0, 0};
-
+    
     const Camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus, 0.0, 1.0);
-
+    
     for (int j = image_height - 1; j >= 0; --j)
     {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
